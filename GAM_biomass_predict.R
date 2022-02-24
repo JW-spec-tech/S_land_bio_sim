64f6a88f947bb036d85e91e1e94a0b66a6dd71d9
JW-spec-tech <joesell@outlook.com>
2022-02-24 12:50:18 -0500

    Added CI calculations

--- GAM_biomass_predict.R
+++ GAM_biomass_predict.R
@@ -1,4 +1,6 @@
-
+library(mgcv)
+library(tidyr)
+library(sspm)
 # Gam_prediction <-function(path="PB.fall.dat",size=500)
 
 # 1. Load the biomass sample data & generate new prediction grid
@@ -18,3 +20,45 @@ spatialonly_gam <- gam(biomass ~s(long, lat, bs="tp"), data= sample, method="REM
 dat_grid <- as.data.frame(expand_grid(long=seq(0,1,length=500), lat= seq(0,1,length=500),year=c(1991:2000)))%>%
   dplyr::mutate(fit_simple_gam  = predict.gam(simple_gam,type = "response",newdata = .),
                 fit_spatialonly_gam = predict.gam(spatialonly_gam,type = "response",newdata = .))
+
+
+
+
+library(mgcv)
+library(readr)
+library(sspm)
+library(tidyr)
+
+trawl_data <- read_table("data/PB_fall.dat")
+head(trawl_data)
+
+simple_gam <- gam(biomass ~
+                    te(long, lat, year, bs= c("tp", "tp"), d = c(2,1)),
+                  data = trawl_data, method="REML", family = "tw")
+
+predict(simple_gam)
+
+# spatialonly_gam <- gam(biomass ~s(long, lat, bs="tp"),
+#                        data = trawl_data, method="REML", family = "tw")
+
+dat_grid <- as.data.frame(expand_grid(long=seq(0,1,length=500),
+                                      lat= seq(0,1,length=500),
+                                      year=c(1996:2000)))
+
+predict_intervals <- sspm:::predict_productivity_intervals
+produce_sims <- sspm:::produce_sims
+find_quantiles <- sspm:::find_quantiles
+confidence_interval <- sspm:::confidence_interval
+prediction_interval <- sspm:::prediction_interval
+
+predict(simple_gam, newdata = dat_grid, type = "lpmatrix")
+
+dat_grid_pred <- dat_grid %>%
+  dplyr::mutate(fit_simple_gam = predict.gam(simple_gam,type = "response",
+                                             newdata = .)) %>%
+  dplyr::bind_cols(predict_intervals(object_fit = simple_gam, new_data = .)) %>%
+  dplyr::group_by(year) %>%
+  dplyr::summarise(fit_simple_gam = sum(fit_simple_gam),
+                   CI_upper = sum(CI_upper), CI_lower = sum(CI_lower)) %>%
+  dplyr::ungroup()
+
