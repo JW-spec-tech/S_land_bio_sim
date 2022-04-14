61c55f1d9a55b31b702f7ad938d2fece382c7fe0
JW-spec-tech <joesell@outlook.com>
2022-04-14 15:47:55 -0400

    add sys time

--- Run_simulatin_generate_data_stratum_local.domain_dat_50test.R
+++ Run_simulatin_generate_data_stratum_local.domain_dat_50test.R
@@ -20,8 +20,9 @@ source(file = "Make_PB_fall.dat.R")
 #### 1. Run the sim ####
 memory.limit(4000)
 
+Sys.time()
 results <- S_land_bio_sim(50,500,500,50,5,0.2,0.5,variation = 2) # highr variation = increased biomass variation
-
+Sys.time()
 #### 2. Write the ogmap files ####
 # Make_patch_domain_arena_DAT(patches=results$patches_list$patches,the_stack=results$the_stack)
 
