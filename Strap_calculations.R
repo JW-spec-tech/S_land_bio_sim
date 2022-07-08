0ae0ce413c7fff1da745bc132097c7bee7af3803
JW-spec-tech <joesell@outlook.com>
2022-07-08 18:52:58 +0000

    Update STRAP -> fix scaling

--- Strap_calculations.R
+++ Strap_calculations.R
@@ -3,6 +3,7 @@
 #### Packages ####
 library(dplyr)
 library(readr)
+library(ggplot2)
 
 #### 1. Load data ####
 survey_raw_data <- readr::read_table("PB_fall.dat")
@@ -23,9 +24,11 @@ Strap_estimate <- Survey_W_Area %>%
   dplyr::group_by(year,stratum)%>%
   
   dplyr::mutate(biomass=biomass/1000) %>% 
-
+  
   dplyr::summarize(Bj = patch_area*mean(biomass),
             s2j = (patch_area^2)*var(biomass)/(n())) %>% 
+  
+  distinct(year, stratum, .keep_all = TRUE) %>% 
   
   dplyr::group_by(year)%>%
   
@@ -33,3 +36,22 @@ Strap_estimate <- Survey_W_Area %>%
   
   dplyr::mutate(lower = B_total - 1.96*B_se,
          upper = B_total + 1.96*B_se)
+
+
+#### 4. plot ####
+
+Strap_estimate <- dplyr::left_join(Strap_estimate,biomass_year)
+
+data_Graph <- ggplot(Strap_estimate, aes(year))
+
+CI_plot <- data_Graph +
+  geom_ribbon(aes(ymin = lower, ymax = upper), fill = "blue", alpha=0.25)+
+  geom_line(aes(y=B_total, colour = "blue"))+
+  geom_line(aes(y=t_bio, colour = "black"))+
+  scale_color_manual(name= "Biomass", labels = c("STRAP","Real"),values = c("blue","black"))+
+  labs(title = "Coverage of Confidence Intervals
+       GAM VS OGmap", x="Year (simulation #)", y="Biomass in kg")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+print(CI_plot)
+
