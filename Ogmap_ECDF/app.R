a8f2fe544ce4bc2fb7764c4d41532d38898c746a
JW-spec-tech <joesell@outlook.com>
2023-03-20 12:12:43 -0400

    fix saving

--- Ogmap_ECDF/app.R
+++ Ogmap_ECDF/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
 library(ggplotify)
+library(ggpubr)
 
 # Define UI
 ui <- fluidPage(
@@ -35,38 +36,49 @@ server <- function(input, output) {
     data.frame(x = x, y = y)
   })
   
-  # First plot: quadratic equation right skewed with sin
-  output$plot1 <- renderPlot({
-    ggplot(quadratic_eqn(), aes(x, y)) +
+  generate_plots <- function(quadratic_eqn_data) {
+    # First plot: quadratic equation right skewed with sin
+    p1 <- ggplot(quadratic_eqn_data, aes(x, y)) +
       geom_point() +
       geom_smooth() +
-      theme_minimal()+
-      ggtitle("Data Points")
-  })
-  
-  
-  # Second plot: ecdf step plot
-  output$plot2 <- renderPlot({
-    ecdf_df <- data.frame(x = sort(quadratic_eqn()$y), y = ecdf(quadratic_eqn()$y)(sort(quadratic_eqn()$y)))
-    ggplot(ecdf_df, aes(x, y)) +
+      theme_minimal() +
+      ggtitle("Data points for plotting")
+    
+    # Second plot: ecdf step plot
+    ecdf_df <- data.frame(x = sort(quadratic_eqn_data$y), y = ecdf(quadratic_eqn_data$y)(sort(quadratic_eqn_data$y)))
+    p2 <- ggplot(ecdf_df, aes(x, y)) +
       geom_step(aes(colour = "ECDF"), size = 1.2) +
       theme_minimal() +
       ggtitle("ECDF Step Plot")
-  })
-  
-  # Third plot: ecdf step plot with adjustable bandwidth and number of steps
-  output$plot3 <- renderPlot({
+    
+    # Third plot: ecdf step plot with adjustable bandwidth and number of steps
     bw <- input$bw
     n_steps <- input$steps
-    ecdf_df <- data.frame(x = sort(quadratic_eqn()$y), y = ecdf(quadratic_eqn()$y)(sort(quadratic_eqn()$y)))
     ecdf_smooth <- approxfun(ecdf_df$x, ecdf_df$y, method = "linear")
     x_smooth <- seq(min(ecdf_df$x), max(ecdf_df$x), length.out = n_steps)
     y_smooth <- ecdf_smooth(x_smooth)
     ecdf_smooth_df <- data.frame(x_smooth, y_smooth)
-    ggplot(ecdf_df, aes(x, y)) +
+    p3 <- ggplot(ecdf_df, aes(x, y)) +
       geom_step(data = ecdf_smooth_df, aes(x = x_smooth, y = y_smooth), size = 1.2, colour = "red") +
       theme_minimal() +
-      ggtitle("ECDF Step Plot with Adjustable Steps")
+      ggtitle("ECDF Step Plot with Adjustable Bandwidth and Steps")
+    
+    list(p1 = p1, p2 = p2, p3 = p3)
+  }
+  
+  output$plot1 <- renderPlot({
+    plot_objects <- generate_plots(quadratic_eqn())
+    plot_objects$p1
+  })
+  
+  output$plot2 <- renderPlot({
+    plot_objects <- generate_plots(quadratic_eqn())
+    plot_objects$p2
+  })
+  
+  output$plot3 <- renderPlot({
+    plot_objects <- generate_plots(quadratic_eqn())
+    plot_objects$p3
   })
   
   output$download_plots <- downloadHandler(
@@ -74,23 +86,19 @@ server <- function(input, output) {
       paste0("plots_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".jpg")
     },
     content = function(file) {
-      # p1 <- output$plot1
-      p2 <- output$plot2
-      p3 <- output$plot3
+      plot_objects <- generate_plots(quadratic_eqn())
+      p1 <- plot_objects$p1
+      p2 <- plot_objects$p2
+      p3 <- plot_objects$p3
       
       # Combine the plots into a single plot
-      combined_plot <- ggarrange(p1(), p2(), p3(), ncol = 1, nrow = 3)
+      combined_plot <- ggarrange(p1, p2, p3, ncol = 1, nrow = 3)
       
       # Save the combined plot as a jpg file
       ggsave(file, plot = combined_plot, device = "jpeg", width = 7, height = 15, dpi = 300)
     },
     contentType = "image/jpeg"
   )
-  
-  
-  
-  
-  
 }
 
 # Run the application
